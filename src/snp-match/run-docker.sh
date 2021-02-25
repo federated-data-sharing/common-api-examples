9021aa5508be79e5cc27a02cb62e14c5c9844e53
rodrigo.barnes@aridhia.com <rodrigo.barnes@aridhia.com>
2021-02-25 17:14:56 +0000

    First pass at snp-match task definition

---
+++ src/snp-match/run-docker.sh
@@ -0,0 +1,6 @@
+rm output/*
+
+docker run -it\
+     --mount type=bind,source="`realpath $(pwd)/input`",target=/mnt/input\
+     --mount type=bind,source="`realpath $(pwd)/output`",target=/mnt/output\
+     snp-match:latest
---
+++ src/snp-match/run-local.sh
@@ -0,0 +1,7 @@
+export CA_INPUT_FOLDER=./input
+export CA_OUTPUT_FOLDER=./output
+export CA_CONFIG_FOLDER=.
+
+rm output/*
+
+python snp-match.py
